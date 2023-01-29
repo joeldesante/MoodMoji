af24fc157a1b98db13abab37a73981cd43002c53
Joel DeSante <joeldesante@gmail.com>
2023-01-29 16:51:23 -0500

    Initial Commit

---
+++ moodmoji/moodmojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  moodmojiApp.swift
+//  moodmoji
+//
+//  Created by Joel DeSante on 1/29/23.
+//
+
+import SwiftUI
+
+@main
+struct moodmojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
